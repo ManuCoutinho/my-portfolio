d07af3a81fb2b157ddecb2c29a83fed05321ab2e
Emanuela Coutinho <gdn.sasha@gmail.com>
2021-06-10 01:15:51 -0300

    atualizando branch

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["DEVELOPER", "FRONT-END", "MANU"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 2000; 
 let textArrayIndex = 0;
 let charIndex = 0;
 
