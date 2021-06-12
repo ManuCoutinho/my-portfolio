1fe49e7725b6462bc89e8766071cfa00213cf3f0
Emanuela Coutinho <gdn.sasha@gmail.com>
2021-06-11 21:12:04 -0300

    pequenas melhorias em responsividade

--- javascript/app.js
+++ javascript/app.js
@@ -36,7 +36,7 @@ function erase() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+document.addEventListener("DOMContentLoaded", function() { 
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
