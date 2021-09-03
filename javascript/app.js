43ca771be3011f87c7df597341004892c5f110e4
ManuCoutinho <gdn.sasha@gmail.com>
2021-09-02 23:52:10 -0300

    Início da refatoração n/ alteração de paleta de cores/fonte/arquivos

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["DEVELOPER", "FRONT-END", "MANU"];
+const textArray = ["DEVELOPER", "FRONT-END", "WOMAN", "MANU"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; 
