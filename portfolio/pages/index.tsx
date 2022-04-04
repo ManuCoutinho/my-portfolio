47f910150adfce317b637937400afc97f3c535ae
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-03 23:14:06 -0300

    :sparkles: new version: created animations main banner

--- portfolio/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1>hello 2.0</h1>
-  )
-}
---
+++ portfolio/src/components/Banner/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  width: 100%;
+  height: 50vh;
+  background: #c6c6c6;
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+`
+
+export const DivWrapper = styled.div`
+  padding: 2rem;
+  width: 100%;
+
+  font-size: 2.8em;
+  font-weight: 900;
+  line-height: 2.5rem;
+  margin: 0 auto;
+
+  p {
+    margin-bottom: 0.5em;
+    font-weight: 100;
+  }
+  span {
+    text-transform: uppercase;
+  }
+`
