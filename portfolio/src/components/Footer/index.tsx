6fcb8c56273acc288e7f0e402d3c3623a28a3b8d
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-07 23:03:41 -0300

    :sparkles: added FOOTER

---
+++ portfolio/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { FooterContainer } from './styles'
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+      <p>Copyright © 2022 Todos os direitos reservados. </p>
+      <p>
+        Designed by <span>Manu Coutinho.</span>
+      </p>
+    </FooterContainer>
+  )
+}
