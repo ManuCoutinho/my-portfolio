5dc47689fc7d408db5bac3e213758a7cbfc461ea
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-15 16:34:27 -0300

    setup debug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
-const debug = process.env.NODE_ENV !== 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: isProd ? 'manucoutinho.github.io/' : ''
 }
--- src/contexts/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SwitchThemeContextProvider } from './SwitchThemeContext'
-
-const GlobalContext: React.FC = ({ children }) => {
-  return <SwitchThemeContextProvider>{children}</SwitchThemeContextProvider>
-}
-
-export default GlobalContext
