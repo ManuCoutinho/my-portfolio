d3833820f42c43cd85391e37df528e6930bd336d
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-18 22:00:01 -0300

    SEO: added main META tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -53,6 +53,26 @@ class MyDocument extends Document {
             href='/assets/favicon.png'
             type='image/png'
           />
+          <link rel='canonical' href='https://manucoutinho.github.io/' />
+          <meta
+            name='keywords'
+            content='webdeveloper, web, developer, programador, desenvolvedor, manu coutinho, front-end, freelancer, freela, website, portfolio, ManuCoutinho, Emanuela Coutinho, react, html, inteface, site, site pessoal'
+          />
+          <meta name='copyright' content='ManuCoutinho 2022' />
+          <meta http-equiv='content-language' content='pt-br' />
+          <meta name='language' content='pt-br' />
+          <meta name='theme-color' content='#EAAB99' />
+          <meta property='og:title' content='Portfólio' />
+          <meta
+            property='og:description'
+            content='Portfólio - Manu Coutinho | Desenvolvedora Front-End'
+          />
+          <meta property='og:image' content='https://imgur.com/a/dYFlfO4' />
+          <meta name='author' content='Emanuela Coutinho' />
+          <meta
+            name='description'
+            content='Portfólio de Emanuela Coutinho, Front-end developer. A Eu sou uma desenvolvedora front-end, movida por uma paixão em construir coisas capazes de integrar diversos universos por meio da tecnologia.'
+          />
         </Head>
         <body>
           <Main />
