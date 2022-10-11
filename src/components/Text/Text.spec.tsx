25998fb69619ddd3473c57ad7a868080e041a0e2
ManuCoutinho <gdn.sasha@gmail.com>
2022-10-11 18:34:27 -0300

    ci: added new env release workflow

--- src/components/Text/Text.spec.tsx
+++ src/components/Text/Text.spec.tsx
@@ -3,8 +3,13 @@ import { render } from '../../styles/theme/renderTheme'
 import { Text } from '.'
 
 describe('<Text/>', () => {
-  it('should render ', () => {
+  it('should render correctly', () => {
     render(<Text>Text</Text>)
     expect(screen.getByText('Text')).toBeInTheDocument()
+  })
+  it('should render with text align prop', () => {
+    const text = render(<Text align='center'>Text</Text>)
+    debug(text)
+    //  expect(screen.getByText('Text')).toHaveStyle({"text-align: center"})
   })
 })
