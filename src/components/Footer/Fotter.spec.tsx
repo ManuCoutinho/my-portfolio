f8f264344b88d85dc085055899e8821746734ba0
ManuCoutinho <gdn.sasha@gmail.com>
2022-10-12 21:52:51 -0300

    refactor: update footer component

--- src/components/Footer/Fotter.spec.tsx
+++ src/components/Footer/Fotter.spec.tsx
@@ -1,12 +1,13 @@
+import { screen } from '@testing-library/react'
 import { render } from '../../styles/theme/renderTheme'
 import Footer from '.'
 
 describe('<Footer/>', () => {
   it('should render a footer component correctly', () => {
     const { container } = render(<Footer />)
-    expect(container.firstChild).toHaveTextContent(
+    expect(screen.getByTestId('footer')).toHaveTextContent(
       /Copyright © 2022 Todos os direitos reservados./i
     )
-    expect(container).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
