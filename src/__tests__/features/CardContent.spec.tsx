42cb63d94c16825e03ffef11b2817dbf992df6aa
ManuCoutinho <gdn.sasha@gmail.com>
2022-12-04 13:03:46 -0300

    test: :test_tube: update tests

--- src/__tests__/features/CardContent.spec.tsx
+++ src/__tests__/features/CardContent.spec.tsx
@@ -1,23 +1,28 @@
 import { screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import { render } from '../../styles/theme/renderTheme'
 import mockData from '../../__mocks__/data'
 import { CardContent } from '../../features/CardContent'
 
 describe('<CardContent />', () => {
   it('should render CardContent section components correctly', () => {
-    render(<CardContent {...mockData} />)
-    expect(
-      screen.getByRole('heading', { name: mockData.name })
-    ).toBeInTheDocument()
+    act(async () => {
+      render(<CardContent {...mockData} />)
+      expect(
+        await screen.findByRole('heading', { name: mockData.name })
+      ).toBeInTheDocument()
+    })
   })
   it('should render tabs components', () => {
-    render(<CardContent {...mockData} />)
-    expect(
-      screen.getByRole('tab', { name: /visão geral/i })
-    ).toBeInTheDocument()
-    expect(screen.getByRole('tab', { name: /detalhes/i })).toBeInTheDocument()
-    expect(screen.getByRole('img', { name: /img1/i })).toBeInTheDocument()
-    expect(screen.getByRole('tabpanel')).toBeInTheDocument()
+    act(async () => {
+      render(<CardContent {...mockData} />)
+      expect(
+        await screen.findByRole('tab', { name: /visão geral/i })
+      ).toBeInTheDocument()
+      expect(screen.getByRole('tab', { name: /detalhes/i })).toBeInTheDocument()
+      expect(screen.getByRole('img', { name: /img1/i })).toBeInTheDocument()
+      expect(screen.getByRole('tabpanel')).toBeInTheDocument()
+    })
   })
   it('should match to snapshot', () => {
     const { container } = render(<CardContent {...mockData} />)
--- src/__tests__/features/CardCover.spec.tsx
+++ src/__tests__/features/CardCover.spec.tsx
@@ -1,12 +1,16 @@
 import { screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import { render } from '../../styles/theme/renderTheme'
 import { mockImg } from '../../__mocks__/images'
 import CardContentCover from '../../features/CardContentCover'
 
-describe('<CardContenCover />', () => {
+describe('<CardContentCover />', () => {
   it('should render CardContentCover section components correctly', () => {
-    render(<CardContentCover img={mockImg} />)
-    expect(screen.getByTestId('stackbox')).toBeInTheDocument()
+    act(async () => {
+      render(<CardContentCover img={mockImg} />)
+      const box = await screen.findByTestId('stackbox')
+      expect(box).toBeInTheDocument()
+    })
   })
   it('should match to snapshot', () => {
     const { container } = render(<CardContentCover />)
