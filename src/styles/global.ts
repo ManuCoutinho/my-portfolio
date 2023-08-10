15231dbcf1b2bc1ec2d96f4f1906f4bad763f0f9
Emanuela Coutinho <gdn.sasha@gmail.com>
2023-08-10 20:59:50 -0300

    update production view (#87)
    * npm prod(deps): bump styled-components from 5.3.6 to 6.0.1
    
    Bumps [styled-components](https://github.com/styled-components/styled-components) from 5.3.6 to 6.0.1.
    - [Release notes](https://github.com/styled-components/styled-components/releases)
    - [Commits](https://github.com/styled-components/styled-components/compare/v5.3.6...v6.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: styled-components
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * :sparkles: create v2 (#74)
    
    * fix: gtag script
    
    * chore: :package: update dependencies
    
    * style: :art: update theme color
    
    * index on feat/v2: 1782986 style: :art: update theme color
    
    * refactor: :recycle: ui v2
    
    * test: added full coverage
    
    * chore: update workflow
    
    * :books: update license file
    
    * :books: update readme file
    
    * :sparkles: added new projects (#79)
    
    * update workflow
    
    * :recycle: update toggle theme
    
    * :sparkles: create intro page
    
    * added new projects data
    
    * fix description mq
    
    * update assets
    
    * :sparkles: new navbar (#82)
    
    * :sparkles: create new header module
    
    * :saprkles: update animation colors
    
    * fix mobile issues
    
    * fix skill carousel
    
    * fix meadia queries (#86)
    
    * :sparkles: new navbar (#83)
    
    * npm prod(deps): bump styled-components from 5.3.6 to 6.0.1
    
    Bumps [styled-components](https://github.com/styled-components/styled-components) from 5.3.6 to 6.0.1.
    - [Release notes](https://github.com/styled-components/styled-components/releases)
    - [Commits](https://github.com/styled-components/styled-components/compare/v5.3.6...v6.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: styled-components
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * :sparkles: create v2 (#74)
    
    * fix: gtag script
    
    * chore: :package: update dependencies
    
    * style: :art: update theme color
    
    * index on feat/v2: 1782986 style: :art: update theme color
    
    * refactor: :recycle: ui v2
    
    * test: added full coverage
    
    * chore: update workflow
    
    * :books: update license file
    
    * :books: update readme file
    
    * :sparkles: added new projects (#79)
    
    * update workflow
    
    * :recycle: update toggle theme
    
    * :sparkles: create intro page
    
    * added new projects data
    
    * fix description mq
    
    * update assets
    
    * :sparkles: new navbar (#82)
    
    * :sparkles: create new header module
    
    * :saprkles: update animation colors
    
    * fix mobile issues
    
    * fix skill carousel
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: layout brakpoints issues
    
    * fix layout issues
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,16 +13,23 @@ html {
 	@media (max-width: 67.5em){
 		font-size: 93.75%;
 	}
+	@media (max-width: 67.5em){
+		font-size: 93.75%;
+	}
 
+	@media (max-width: 50em){
+		font-size:85%;
+	}
 	@media (max-width: 50em){
 		font-size:85%;
 	}
   
 	@media (max-width: 27em) {
-  html {
-    font-size: 80%;
-  }
-}
+		html {
+			font-size: 80%;
+		}
+	}
+
 }
 
 body, html {
@@ -38,19 +45,26 @@ body {
 		transition: ${theme.transition};
 		&::-webkit-scrollbar {
 			width: 0.5rem;
+			width: 0.5rem;
 		}
 
 		//* Track *//
+		//* Track *//
 		&::-webkit-scrollbar-track {
-			background: ${theme.colors.white};
+			background: ${theme.colors.body};
 		}
+		//*Handle *//
 		//*Handle *//
 		&::-webkit-scrollbar-thumb {
 			background: ${lighten(0.05, theme.colors.accent)};
 			border-radius: ${theme.radius.sm};
+			background: ${lighten(0.05, theme.colors.accent)};
+			border-radius: ${theme.radius.sm};
 		}
 		//*Handle on hover *//
+		//*Handle on hover *//
 		&::-webkit-scrollbar-thumb:hover {
+			background: ${lighten(0.1, theme.colors.body)};
 			background: ${lighten(0.1, theme.colors.body)};
 		}
 	`}
@@ -72,6 +86,9 @@ h1, h2, h3, h4, h5, h6, strong {
 }
 
 a {
+	text-decoration: none;
+	color: inherit;
+	cursor: pointer;  
 	text-decoration: none;
 	color: inherit;
 	cursor: pointer;  
@@ -115,4 +132,5 @@ video[width] {
   outline: 0;
 }
 `
+
 export default GlobalStyle
